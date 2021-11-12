ebaf3ac1a345a6371de404b44aa87a3ddf163e87
katarzyna_w <katarzyna.koch@gmail.com>
2021-11-12 11:43:47 -0600

    mobile styling for Text component

--- src/components/Text.component.jsx
+++ src/components/Text.component.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import '../App.css'
+import './Text.css'
 
 function Text() {
    return (
-      <div>
-         <p>We're coming soon</p>
-
-         <p>Hello fellow shoppers! We're currently building our new fashion store. 
-         Add your email below to stay up-to-date with announcements and our launch deals.</p>
+      <div className="text-wrapper">
+         <h1 className="text-greeting__light">
+            We're
+         </h1> 
+         <h1 className="text-greeting__bold">
+            coming soon
+         </h1>
+         <p className="text-body">Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.</p>
 
 </div>
    )
