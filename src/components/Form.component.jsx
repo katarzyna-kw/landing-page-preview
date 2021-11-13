52bf5852703b89ed5f7f474fd9416491ec095c1b
katarzyna_w <katarzyna.koch@gmail.com>
2021-11-13 10:31:36 -0600

    mobile button styling with shadow and border

--- src/components/Form.component.jsx
+++ src/components/Form.component.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css'
+import './Form.css'
 
 function Form() {
+
+   const [email, setEmail] = useState('');
+
+   const validateEmail = (e) => {
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(String(email).toLowerCase());
+   }
+  
    return (
-      <div>
-         <p>Email Address</p>
+      <div class="form-wrapper">
+         <form>
+            <input 
+               value={email}
+               placeholder="Email Address"
+               type="text" 
+               name="email"
+               onChange={e => setEmail(e.target.value)}
+            /> 
+            <button>
+               <img src="https://raw.githubusercontent.com/katarzyna-kw/landing-page-preview/d39487d8743a0fb56e66db0dc6f974936e478f71/public/images/icon-arrow.svg" alt="click to submit email address" placeholder={email} />
+            </button>        
+            </form>
       </div>
    )
 }
